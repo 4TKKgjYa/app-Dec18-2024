3ea282c69937f665cd273c6519c8b6f71d79e9cb
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-19 23:56:26 +0900

    tailwindcssのインストール

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,8 @@
-import type { MetaFunction, LoaderFunction } from "@remix-run/cloudflare";
+import type { MetaFunction, LoaderFunction, LinksFunction } from "@remix-run/cloudflare";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
 import { ClerkApp } from "@clerk/remix";
+import stylesheet from "~/tailwind.css?url";
 
 export const meta: MetaFunction = () => ([{
   charset: "utf-8",
@@ -10,6 +11,10 @@ export const meta: MetaFunction = () => ([{
 }]);
 
 export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
