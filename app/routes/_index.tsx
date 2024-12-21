b405114b0cb2cc11cdfaf2be21d48472d1d3b98e
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-22 00:56:00 +0900

    ヘッダーの基本が完成

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,5 @@
 import {
   SignInButton,
-  SignOutButton,
-  SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
@@ -10,27 +8,15 @@ import {
 export default function Index() {
   return (
     <>
-      <SignedIn>
-        <p>You are signed in!</p>
-        <div>
-          <p>View your profile here</p>
+      <header className="flex justify-between">
+        <h1>My App</h1>
+        <SignedIn>
           <UserButton />
-        </div>
-        <div>
-          <SignOutButton />
-        </div>
-      </SignedIn>
-      <SignedOut>
-        <p>You are signed out</p>
-        <div>
-          <SignInButton>
-          <button className="inline-flex h-8 items-center justify-center rounded-md bg-neutral-950 px-4 font-medium text-neutral-50 transition active:scale-110 ">click me</button>
-          </SignInButton>
-        </div>
-        <div>
-          <SignUpButton />
-        </div>
-      </SignedOut>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </header>
     </>
   );
 }
