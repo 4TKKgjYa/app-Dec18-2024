114e5a648904dfbef24b5085ac8a781734f184c6
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-22 09:56:09 +0900

    レスポンシブデザインに対応

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,6 @@ import stylesheet from "~/tailwind.css?url";
 export const meta: MetaFunction = () => ([
   { charset: "utf-8" },
   { title: "New Remix App" },
-  { viewport: "width=device-width,initial-scale=1" },
 ]);
 
 export const loader: LoaderFunction = (args) => rootAuthLoader(args);
@@ -20,6 +19,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
