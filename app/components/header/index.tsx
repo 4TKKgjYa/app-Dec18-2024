d4649fb9609446ed86b3193e9e753eeff0a825c5
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-24 00:11:53 +0900

    Headerコンポーネントの調整

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -11,14 +11,23 @@ type HeaderProps = {
 
 export const Header = ({ title }: HeaderProps) => {
   return (
-    <>
-      <h1>{title}</h1>
+    <header className="flex justify-between h-[52px] px-2 bg-blue-900 ss:bg-blue-900 text-white">
+      <h1 className="flex items-center cursor-default">{title}</h1>
       <SignedIn>
         <UserButton />
       </SignedIn>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <div className="flex items-center">
+            <button
+              type="button"
+              className="h-[36px] inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </SignInButton>
       </SignedOut>
-    </>
+    </header>
   );
 }
