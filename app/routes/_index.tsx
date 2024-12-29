916344926c4d1c19bcc3f756b78d19dad956707c
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-29 15:03:45 +0900

    feature: Supabaseとの接続に成功

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,39 @@
 import { SignedIn, SignedOut } from "@clerk/remix";
 import { Header } from "app/components/header";
+import { useLoaderData } from "@remix-run/react";
+import { createClient } from '@supabase/supabase-js';
+
+type Country = {
+  id: number;
+  name: string;
+  iso2: string;
+  iso3: string;
+  local_name: string | null;
+  continents: string | null;
+};
+
+export async function loader(): Promise<{ countries: Country[]; }> {
+  const client = createClient(
+    process.env.SUPABASE_URL || '',
+    process.env.SUPABASE_ANON_KEY || ''
+  );
+
+  const { data, error } = await client.from("countries").select("*");
+
+  if (error) {
+    throw error;
+  }
+
+  return {
+    countries: data,
+  };
+}
 
 export default function Index() {
+  const countries = useLoaderData<typeof loader>();
+
+  console.log(countries);
+  
   return (
     <div className="">
       <Header title="My App" />
