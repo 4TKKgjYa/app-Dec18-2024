9226dd14239ca18dd5f34bf40616db4bc7be6325
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-31 10:49:24 +0900

    hotfix: 環境変数をべた書きに変更

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,8 @@ type Country = {
 
 export async function loader(): Promise<{ countries: Country[]; }> {
   const client = createClient(
-    process.env.SUPABASE_URL || '',
-    process.env.SUPABASE_ANON_KEY || ''
+    "https://ikcorsyuoetdfhnpmvqq.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrY29yc3l1b2V0ZGZobnBtdnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0MjMyMzYsImV4cCI6MjA1MDk5OTIzNn0.8meP5n132pcnVEWHAq7r4881QOX28skEi1W0Potc5NI"
   );
 
   const { data, error } = await client.from("countries").select("*");
