2457955c46caf30b6a4490a2a910a23285760b14
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2024-12-31 11:18:43 +0900

    hotfix: 環境変数をimport.meta.envに変更

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,8 @@ type Country = {
 
 export async function loader(): Promise<{ countries: Country[]; }> {
   const client = createClient(
-    "https://ikcorsyuoetdfhnpmvqq.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrY29yc3l1b2V0ZGZobnBtdnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0MjMyMzYsImV4cCI6MjA1MDk5OTIzNn0.8meP5n132pcnVEWHAq7r4881QOX28skEi1W0Potc5NI"
+    import.meta.env.SUPABASE_URL,
+    import.meta.env.SUPABASE_ANON_KEY
   );
 
   const { data, error } = await client.from("countries").select("*");
